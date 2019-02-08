befcaa3b77cfa6537d6e05dd92e2c13881cc87cc
amrithm98 <amrithchalleger@gmail.com>
2019-02-08 18:52:14 +0530

    Completed experiment 3 code

--- cycle1/experiment3/processThread.cpp
+++ cycle1/experiment3/processThread.cpp
@@ -1,7 +1,22 @@
-#include<>
+#include<thread>
+#include<iostream>
+#include<unistd.h>
 using namespace std;
+void threadcb(int id){
+    if(id == 0)
+        cout<<"This thread was created by child process and thread id is \n"<<
+        this_thread::get_id()<<"\n";
+    else
+        cout<<"This thread was created by the parent process and thread id is 
+        \n"<<this_thread::get_id()<<"\n";
+    
+}
 
 int main(){
-    
+    int threadno = fork();
+    thread thread1(threadcb,threadno);
+    thread thread2(threadcb,threadno);
+    thread1.join();
+    thread2.join();
     return 0;
 }
